3ac16d6445cba3e43ce85001a450711d62197823
lifangjie <lifangjie@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2013-12-09 08:52:37 +0000

    add thrift
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@97341 94b0661d-e41e-4e49-82e3-1d5c8131657a

---
+++ src/main/java/com/yy/cs/base/thrift/invoker/InvokerFacoty.java
@@ -0,0 +1,22 @@
+package com.yy.cs.base.thrift.invoker;
+
+import org.apache.commons.pool.ObjectPool;
+
+import com.duowan.pooling.IObjectPoolManager;
+import com.yy.cs.base.thrift.transport.Client;
+
+
+public class InvokerFacoty {
+	
+	
+	private InvokerFacoty(){
+	}
+	
+	
+	public static Invoker getInvoker(IObjectPoolManager<? extends ObjectPool, Client> objPoolMgr){
+		return new Invoker(objPoolMgr);
+	}
+	
+	
+	 
+}
