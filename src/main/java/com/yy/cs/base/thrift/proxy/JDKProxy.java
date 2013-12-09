f5203e503fb3a24e641d1f1942133cd3643ff585
lifangjie <lifangjie@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2013-12-09 06:19:10 +0000

    add thrift
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@97258 94b0661d-e41e-4e49-82e3-1d5c8131657a

---
+++ src/main/java/com/yy/cs/base/thrift/proxy/JDKProxy.java
@@ -0,0 +1,16 @@
+package com.yy.cs.base.thrift.proxy;
+
+import com.yy.cs.base.thrift.protocol.Invoker;
+
+
+public class JDKProxy{
+
+	@SuppressWarnings("unchecked")
+	public <T> T getProxy(Invoker invoker, Class<?> interfaces) {
+		return (T) java.lang.reflect.Proxy.newProxyInstance(Thread.currentThread()
+				.getContextClassLoader(), new Class<?>[]{interfaces},
+				new ThriftInvocationHandler(invoker));
+	}
+
+	
+}
