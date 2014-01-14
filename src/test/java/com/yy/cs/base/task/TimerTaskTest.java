785b67db8e61f74939a6051cdce516dc5c7c1921
liuxu <liuxu@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2014-01-14 10:22:58 +0000

    测试所用的timeTask修改
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@102986 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/test/java/com/yy/cs/base/task/TimerTaskTest.java
+++ src/test/java/com/yy/cs/base/task/TimerTaskTest.java
@@ -1,13 +1,27 @@
 package com.yy.cs.base.task;
 
 import java.util.Date;
+import java.util.List;
+
+import com.yy.cs.base.status.CsStatus;
 
 public class TimerTaskTest extends TimerTask {
 	
 	private Date date;
 	@Override
 	public void execute() throws Exception{
-		System.out.println(new Date() + "-----------execute()---TimerTaskTest--------");
+		Thread.sleep(1000);
+		System.out.println(new Date()
+				+ "-----------execute()---TimerTaskTest--------");
+//		date.compareTo(date);
 	}
-	
+
+	public static void main(String[] agrs) {
+		TimerTaskManager timerTaskManager = new TimerTaskManager();
+		TimerTaskTest time = new TimerTaskTest();
+		time.setCron("*/30 * * * * *");
+		timerTaskManager.addTimerTask(time);
+		timerTaskManager.start();
+		CsStatus status = timerTaskManager.getCsStatus();
+	}
 }
