3587ec0dccb375e45e779bc7183e50f9bef4704a
lifangjie <lifangjie@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2014-03-03 08:37:42 +0000

    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@108772 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/test/java/integration/com/yy/cs/base/json/JsonTest.java
+++ src/test/java/integration/com/yy/cs/base/json/JsonTest.java
@@ -1,7 +1,15 @@
 package integration.com.yy.cs.base.json;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yy.cs.base.json.Json;
 import com.yy.cs.base.json.JsonTest.TObj;
 
@@ -38,4 +46,51 @@ public class JsonTest {
 		System.out.println("ObjToStrPerformance. Time cost (ms): " + time
 				+ ", count: " + count + ", tps: " + count * 1000 / time);
 	}
+	private static ObjectMapper mapper = new ObjectMapper();
+	@Test
+	public void testObjToStr() throws JsonParseException, JsonMappingException, IOException { // Intel i5 (linux): ~1.82M TPS
+		
+		String str = "[{\"target\": \"stats.timers.client-update-server.database.time.refreshing.success.115_238_171_223.mean_90\", \"datapoints\": [[3361.0, 1393491600], [3999.0, 1393491660]]}]";
+		List<Bean> lsit = mapper.readValue(str, new TypeReference<List<Bean>>(){});
+		System.out.println(lsit);
+		
+		
+	}
+	public void testStrToObja(){
+		String str = "[{\"target\": \"stats.timers.client-update-server.database.time.refreshing.success.115_238_171_223.mean_90\", \"datapoints\": [[3361.0, 1393491600], [3999.0, 1393491660]]}]";
+		List<Bean> lsit = Json.strToObj(str,new TypeReference<List<Bean>>(){});
+		System.out.println(lsit);
+	}
+	
+	
+}
+
+class Bean{
+	
+	String target;
+	
+	List<List<String>> datapoints;
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+
+	public List<List<String>> getDatapoints() {
+		return datapoints;
+	}
+
+	public void setDatapoints(List<List<String>> datapoints) {
+		this.datapoints = datapoints;
+	}
+
+	@Override
+	public String toString() {
+		return "Bean [target=" + target + ", datapoints=" + datapoints + "]";
+	}
+	
+	
 }
