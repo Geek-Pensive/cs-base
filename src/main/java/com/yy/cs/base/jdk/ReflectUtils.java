887e0aacc2afa4ae0375107730fa767008311d67
tanghengde <tanghengde@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2015-07-01 03:07:40 +0000

    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@247370 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/main/java/com/yy/cs/base/jdk/ReflectUtils.java
+++ src/main/java/com/yy/cs/base/jdk/ReflectUtils.java
@@ -4,6 +4,14 @@ import java.lang.reflect.Field;
 
 public class ReflectUtils {
 
+    /**
+     * 获取类里面的某个字段，在递归获取的情况下，子类定义的字段优先返回
+     * @param clz 类
+     * @param fieldName 字段名
+     * @param recursion 是否递归获取父类的字段
+     * @return
+     * @throws Exception
+     */
     public static Field getClassField(Class<?> clz, String fieldName, boolean recursion) throws Exception {
         Field[] declaredFields = clz.getDeclaredFields();
         for (Field field : declaredFields) {
