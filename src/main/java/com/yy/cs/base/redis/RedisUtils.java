1e218c9d3794fa3f4654ab2ce32769c335c52623
tanghengde <tanghengde@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2015-07-01 03:09:37 +0000

    获取commons-pool的版本区别
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@247372 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/main/java/com/yy/cs/base/redis/RedisUtils.java
+++ src/main/java/com/yy/cs/base/redis/RedisUtils.java
@@ -1,7 +1,10 @@
 package com.yy.cs.base.redis;
 
+import java.lang.reflect.Method;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * 
@@ -9,88 +12,104 @@ import java.util.regex.Pattern;
  *
  */
 public class RedisUtils {
-	
-	/**
-	 * 从jedis返回的info信息中获取connected client 数量
-	 * </br>
-	 * 如果info为空，或者匹配信息不到，则返回 0
-	 * @param info
-	 * 		redis服务器的相关配置信息
-	 * @return 
-	 * 		connected client 数量
-	 */
-	public static int getConnectedClientNum(String info){
-		if(info == null || "".equals(info)){
-			return 0;
-		}
-		Pattern p = Pattern.compile("connected_clients:(\\d+)");
-		Matcher m = p.matcher(info);
-		if(m.find()){
-			return Integer.valueOf(m.group(1));
-		}
-		return 0;
-	}
 
-	/**
-	 * 从jedis返回的info信息中分析 该redis实例是否是 master
-	 * </br>
-	 * 如果info为空，或者匹配信息不到，则返回  false
-	 * @param info
-	 * 		redis服务器的相关配置信息
-	 * @return
-	 * 		boolean
-	 */
-	public static boolean isMaster(String info){
-		if(info == null || "".equals(info)){
-			return false;
-		}
-		Pattern p = Pattern.compile("role:(\\w+)");
-		Matcher m = p.matcher(info);
-		if(m.find()){
-			String isMaster = (m.group(1));
-			return "master".equals(isMaster) ? true : false;
-		}
-		return false;
-	}
+    /**
+     * 从jedis返回的info信息中获取connected client 数量
+     * </br>
+     * 如果info为空，或者匹配信息不到，则返回 0
+     * 
+     * @param info
+     *            redis服务器的相关配置信息
+     * @return
+     *         connected client 数量
+     */
+    public static int getConnectedClientNum(String info) {
+        if (info == null || "".equals(info)) {
+            return 0;
+        }
+        Pattern p = Pattern.compile("connected_clients:(\\d+)");
+        Matcher m = p.matcher(info);
+        if (m.find()) {
+            return Integer.valueOf(m.group(1));
+        }
+        return 0;
+    }
 
-	/**
-	 * @param serverInfo  
-	 * 		格式如  ip:port:password:timeout
-	 * @return 
-	 * 		String数组，分别为ip,port，password,timeout
-	 */
-	public static String[] parseServerInfo(String serverInfo){
-		if(serverInfo == null || "".equals(serverInfo)){
-			throw new CsRedisRuntimeException("invalid param, param couldn't be blank");
-		}
-		String [] str = serverInfo.split(":");
-		for(int i = 0;i < str.length; i++){
-		    if("".equals(str[i])){
-		        str[i] = null;
-		    }
-		}
-		if(str.length < 2){
-			throw new CsRedisRuntimeException("invalid param, param should like ip:port:password:timeout, if not exists password, you can use ip:port::");
-		}
-		String [] result = new String[4];
-		result[0] = str[0];
-		result[1] = str[1];
-		if(str.length == 3){
-			result[2] = str[2];
-		}
-		if(str.length == 4){
-			result[2] = str[2];
-			result[3] = str[3];
-		}
-		return result;
-	}
-	
-//	public static void main(String []args){
-//		String [] strArray = parseServerInfo("172.19.103.105:6331::1000");
-//		for(int i = 0; i < strArray.length; i++){
-//			System.out.println(strArray[2] == null);
-//			System.out.println(i + " " + strArray[i]);
-//		}
-//	}
-	
+    /**
+     * 从jedis返回的info信息中分析 该redis实例是否是 master
+     * </br>
+     * 如果info为空，或者匹配信息不到，则返回 false
+     * 
+     * @param info
+     *            redis服务器的相关配置信息
+     * @return
+     *         boolean
+     */
+    public static boolean isMaster(String info) {
+        if (info == null || "".equals(info)) {
+            return false;
+        }
+        Pattern p = Pattern.compile("role:(\\w+)");
+        Matcher m = p.matcher(info);
+        if (m.find()) {
+            String isMaster = (m.group(1));
+            return "master".equals(isMaster) ? true : false;
+        }
+        return false;
+    }
+
+    /**
+     * @param serverInfo
+     *            格式如 ip:port:password:timeout
+     * @return
+     *         String数组，分别为ip,port，password,timeout
+     */
+    public static String[] parseServerInfo(String serverInfo) {
+        if (serverInfo == null || "".equals(serverInfo)) {
+            throw new CsRedisRuntimeException("invalid param, param couldn't be blank");
+        }
+        String[] str = serverInfo.split(":");
+        for (int i = 0; i < str.length; i++) {
+            if ("".equals(str[i])) {
+                str[i] = null;
+            }
+        }
+        if (str.length < 2) {
+            throw new CsRedisRuntimeException(
+                    "invalid param, param should like ip:port:password:timeout, if not exists password, you can use ip:port::");
+        }
+        String[] result = new String[4];
+        result[0] = str[0];
+        result[1] = str[1];
+        if (str.length == 3) {
+            result[2] = str[2];
+        }
+        if (str.length == 4) {
+            result[2] = str[2];
+            result[3] = str[3];
+        }
+        return result;
+    }
+
+    /**
+     * 判断jedis依赖的commons-pool版本。
+     * jedis2.4.2后，使用的是commons-pool2通过特征字段获取commons-pool的版本，默认是返回1。commons-pool2返回2
+     *
+     * @return
+     */
+    public static int versionOfCommonsPool() {
+        int version = 1;
+        Class<?> jp = JedisPoolConfig.class;
+        try {
+            jp.getMethod("getMaxActive");
+        } catch (NoSuchMethodException e) {
+            version = 2;
+        }
+        return version;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(versionOfCommonsPool());
+    }
+
 }
