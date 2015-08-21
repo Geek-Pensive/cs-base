79462c70c33e02319e15cefd1c5cbc6d1be092cb
tanghengde <tanghengde@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2015-08-21 07:28:22 +0000

    dbIndex
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@260524 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/main/java/com/yy/cs/base/redis/RedisClient.java
+++ src/main/java/com/yy/cs/base/redis/RedisClient.java
@@ -764,12 +764,29 @@ public class RedisClient {
      *         默认db下,当前key对应的所有成员
      */
     public Set<String> smembers(String key) {
+        return smembers(0, key);
+    }
+
+    /**
+     * 执行smembers操作，然后释放client连接
+     * 返回集合 key 中的所有成员。不存在的 key 被视为空集合
+     * 
+     * @param dbIndex
+     * @param key
+     *            set的key值
+     * 
+     * @return
+     *         默认db下,当前key对应的所有成员
+     */
+    public Set<String> smembers(int dbIndex, String key) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisSlavePool();
             jedis = jedisPool.getResource();
-
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.smembers(key);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -794,12 +811,29 @@ public class RedisClient {
      *         被添加到集合中的新元素的数量，不包括被忽略的元素。
      */
     public Long sadd(String key, String... values) {
+        return sadd(0, key, values);
+    }
+
+    /**
+     * redis SADD 操作，向名为key的set中添加一个或者多个value
+     * 
+     * @param dbIndex
+     * @param key
+     *            假如 key不存在，则创建一个只包含 member 元素作成员的集合。当 key 不是集合类型时，返回一个错误。
+     * @param values
+     *            要存入的value
+     * @return
+     *         被添加到集合中的新元素的数量，不包括被忽略的元素。
+     */
+    public Long sadd(int dbIndex, String key, String... values) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisMasterPool();
             jedis = jedisPool.getResource();
-
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.sadd(key, values);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -824,12 +858,29 @@ public class RedisClient {
      *         被删除的元素的个数
      */
     public Long srem(String key, String... values) {
+        return srem(0, key, values);
+    }
+
+    /**
+     * redis的 SREM 操作，移除set(名称为key)中的一个或多个value
+     * 
+     * @param dbIndex
+     * @param key
+     *            set的key值
+     * @param values
+     *            要被删除的value值
+     * @return
+     *         被删除的元素的个数
+     */
+    public Long srem(int dbIndex, String key, String... values) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisMasterPool();
             jedis = jedisPool.getResource();
-
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.srem(key, values);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -854,12 +905,29 @@ public class RedisClient {
      *         set中这个key对应的value的个数
      */
     public Long scard(String key) {
+        return scard(0, key);
+    }
+
+    /**
+     * Redis 的SCARD 操作，返回当前set中的value个数。
+     * 集合的基数。
+     * 当 key 不存在时，返回 0
+     * 
+     * @param dbIndex
+     * @param key
+     *            set的key值
+     * @return Long
+     *         set中这个key对应的value的个数
+     */
+    public Long scard(int dbIndex, String key) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisSlavePool();
             jedis = jedisPool.getResource();
-
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.scard(key);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -885,12 +953,31 @@ public class RedisClient {
      *         boolean值
      */
     public Boolean sismember(String key, String value) {
+        return sismember(key, value);
+    }
+
+    /**
+     * SISMEMBER key member 判断 member 元素是否集合 key 的成员。
+     * 如果 member 元素是集合的成员，返回 1 。
+     * 如果 member 元素不是集合的成员，或 key 不存在，返回 0 。
+     * 
+     * @param dbIndex
+     * @param key
+     *            set的key值
+     * @param value
+     *            被判断的字符串value
+     * @return
+     *         boolean值
+     */
+    public Boolean sismember(int dbIndex, String key, String value) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisSlavePool();
             jedis = jedisPool.getResource();
-
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.sismember(key, value);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -920,12 +1007,33 @@ public class RedisClient {
      *         如果哈希表中域 field 已经存在且旧值已被新值覆盖，返回 0 。
      */
     public Long hset(String key, String field, String value) {
+        return hset(0, key, field, value);
+    }
+
+    /**
+     * 将哈希表 key 中的域 field 的值设为 value 。如果 key 不存在，一个新的哈希表被创建并进行 HSET 操作。
+     * 如果域 field 已经存在于哈希表中，旧值将被覆盖。
+     * 
+     * @param dbIndex
+     * @param key
+     *            hash表的标记key
+     * @param field
+     *            hash表中的存储对象的key
+     * @param value
+     *            hash表中的存储对象的key对应的value
+     * @return
+     *         如果 field 是哈希表中的一个新建域，并且值设置成功，返回 1 。
+     *         如果哈希表中域 field 已经存在且旧值已被新值覆盖，返回 0 。
+     */
+    public Long hset(int dbIndex, String key, String field, String value) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisMasterPool();
             jedis = jedisPool.getResource();
-
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.hset(key, field, value);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -952,12 +1060,31 @@ public class RedisClient {
      *         当 key 不是哈希表(hash)类型时，返回一个错误。
      */
     public String hmset(String key, Map<String, String> value) {
+        return hmset(0, key, value);
+    }
+
+    /**
+     * 同时将多个 field-value (域-值)对设置到哈希表 key 中。此命令会覆盖哈希表中已存在的域。
+     * 如果 key 不存在，一个空哈希表被创建并执行 HMSET 操作。
+     * 
+     * @param dbIndex
+     * @param key
+     *            hash表的标记key
+     * @param value
+     *            多个 field-value (域-值)对
+     * @return
+     *         如果命令执行成功，返回 OK 。
+     *         当 key 不是哈希表(hash)类型时，返回一个错误。
+     */
+    public String hmset(int dbIndex, String key, Map<String, String> value) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
-
             jedisPool = getJedisMasterPool();
             jedis = jedisPool.getResource();
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.hmset(key, value);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -982,12 +1109,29 @@ public class RedisClient {
      *         给定域的值。当给定域不存在或是给定 key 不存在时，返回 nil 。
      */
     public String hget(String key, String field) {
+        return hget(key, field);
+    }
+
+    /**
+     * 返回哈希表 key 中给定域 field 的值。
+     * 
+     * @param dbIndex
+     * @param key
+     *            hash表的标记key
+     * @param field
+     *            hash表中的某个存储空间的标识key
+     * @return
+     *         给定域的值。当给定域不存在或是给定 key 不存在时，返回 nil 。
+     */
+    public String hget(int dbIndex, String key, String field) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisSlavePool();
             jedis = jedisPool.getResource();
-
+            if (dbIndex != 0) {
+                jedis.select(dbIndex);
+            }
             return jedis.hget(key, field);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -1013,12 +1157,31 @@ public class RedisClient {
      *         一个包含多个给定域的关联值的表，表值的排列顺序和给定域参数的请求顺序一样
      */
     public List<String> hmget(String key, String... fields) {
+        return hmget(0, key, fields);
+    }
+
+    /**
+     * 返回哈希表 key 中，一个或多个给定域的值。
+     * 如果给定的域不存在于哈希表，那么返回一个 nil 值。
+     * 因为不存在的 key 被当作一个空哈希表来处理，所以对一个不存在的 key 进行 HMGET 操作将返回一个只带有 nil 值的表。
+     * 
+     * @param dbIndex
+     * @param key
+     *            hash表的标记key
+     * @param fields
+     *            多个给定域的标识fields
+     * @return
+     *         一个包含多个给定域的关联值的表，表值的排列顺序和给定域参数的请求顺序一样
+     */
+    public List<String> hmget(int dbIndex, String key, String... fields) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisSlavePool();
             jedis = jedisPool.getResource();
-
+            if (0 != dbIndex) {
+                jedis.select(dbIndex);
+            }
             return jedis.hmget(key, fields);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -1041,22 +1204,7 @@ public class RedisClient {
      *         Long类型,删除元素个数
      */
     public Long remove(String key) {
-        Jedis jedis = null;
-        JedisPool jedisPool = null;
-        try {
-            jedisPool = getJedisMasterPool();
-            jedis = jedisPool.getResource();
-            return jedis.del(key);
-        } catch (Exception e) {
-            jedisPool.returnBrokenResource(jedis);
-            jedis = null;
-            factory.init();
-            throw new CsRedisRuntimeException("jedis set fail", e);
-        } finally {
-            if (jedis != null) {
-                jedisPool.returnResource(jedis);
-            }
-        }
+        return remove(0, key);
     }
 
     /*** common operation ***/
@@ -1101,13 +1249,31 @@ public class RedisClient {
      *         改名成功时提示 OK ，失败时候返回一个错误。
      */
     public String rename(String oldkey, String newkey) {
+        return rename(0, oldkey, newkey);
+    }
+
+    /**
+     * 将 key 改名为 newkey 。当 key 和 newkey 相同，或者 key 不存在时，返回一个错误。
+     * 当 newkey 已经存在时， RENAME 命令将覆盖旧值。
+     * 
+     * @param dbIndex
+     * @param oldkey
+     *            原有的key
+     * @param newkey
+     *            新命名的key
+     * @return
+     *         改名成功时提示 OK ，失败时候返回一个错误。
+     */
+    public String rename(int dbIndex, String oldkey, String newkey) {
 
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisMasterPool();
             jedis = jedisPool.getResource();
-
+            if (0 != dbIndex) {
+                jedis.select(dbIndex);
+            }
             return jedis.rename(oldkey, newkey);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -1130,12 +1296,27 @@ public class RedisClient {
      *         存在则返回true 否则返回false
      */
     public boolean exists(String key) {
+        return exists(0, key);
+    }
+
+    /**
+     * 判断当前key是否已存在
+     * 
+     * @param dbIndex
+     * @param key
+     *            set中的key值
+     * @return
+     *         存在则返回true 否则返回false
+     */
+    public boolean exists(int dbIndex, String key) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisSlavePool();
             jedis = jedisPool.getResource();
-
+            if (0 != dbIndex) {
+                jedis.select(dbIndex);
+            }
             return jedis.exists(key);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
@@ -1181,12 +1362,25 @@ public class RedisClient {
      *            set中的key值
      */
     public void watch(String key) {
+        watch(0, key);
+    }
+
+    /**
+     * 监视一个(或多个) key ，如果在事务执行之前这个(或这些) key 被其他命令所改动，那么事务将被打断。
+     * 
+     * @param dbIndex
+     * @param key
+     *            set中的key值
+     */
+    public void watch(int dbIndex, String key) {
         Jedis jedis = null;
         JedisPool jedisPool = null;
         try {
             jedisPool = getJedisMasterPool();
             jedis = jedisPool.getResource();
-
+            if (0 != dbIndex) {
+                jedis.select(dbIndex);
+            }
             jedis.watch(key);
         } catch (Exception e) {
             jedisPool.returnBrokenResource(jedis);
