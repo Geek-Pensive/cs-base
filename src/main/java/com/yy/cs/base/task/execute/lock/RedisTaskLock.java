bc0c3a939ab4556c717f3458b15b1f432a6aa66d
tanghengde <tanghengde@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2015-11-05 07:15:18 +0000

    修改获取服务IP信息逻辑。
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@279804 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/main/java/com/yy/cs/base/task/execute/lock/RedisTaskLock.java
+++ src/main/java/com/yy/cs/base/task/execute/lock/RedisTaskLock.java
@@ -20,167 +20,168 @@ import com.yy.cs.base.redis.RedisClient;
  *
  */
 public class RedisTaskLock implements TaskLock {
-	
-	private static final Logger logger = LoggerFactory.getLogger(RedisTaskLock.class);
-	
-	private final RedisClient redisClient; 
-	
-	private static final int defaultExpire = 7 * 24 * 60 * 60;   //失效时间7天
-	
-	private static final String defaultPrefix = "cs-base-task-";   //
-	
-	private static final String defaultSplit = "&";
-	
-	private final int expire;   //
-	/**
-	 * 构造器函数
-	 * @param redisClient
-	 * 		{@link RedisClient} redis的java客户端jedis封装类
-	 */
-	public RedisTaskLock(RedisClient redisClient) {
-		this(redisClient,defaultExpire);
-	}
-	/**
-	 * 构造器函数
-	 * @param redisClient
-	 * 		redisClient对象
-	 * @param expire
-	 * 		锁失效时间
-	 */
-	public RedisTaskLock(RedisClient redisClient, int expire) {
-		super();
-		this.redisClient = redisClient;
-		this.expire = expire;
-	}
-	
-	@Override
-	public boolean lock(String id, long value) {
-		JedisPool pool = null;
-		Jedis	jedis  = null;
-		boolean result = false;
-		try{
-			pool = redisClient.getJedisMasterPool();
-			jedis =	pool.getResource();
-			String key = defaultPrefix + id + defaultSplit + value;
-			String v = getLocalAddress() + defaultSplit + value;  
-			long setnx = jedis.setnx(key, v);
-			//如果等于1 加锁成功
-			result = setnx == 1 ? true : false;
-	        if(result) {
-	        	//设置超时时间
-	        	jedis.expire(key, expire);
-	        	return result;
-	        }
-		}catch(Throwable t){
-			//防御性容错，如果异常直接返回false
-			logger.error(" task id:" + id + " value:" +value , t);
-		}
-		finally{
-			if(pool != null && jedis != null){
-				pool.returnResource(jedis);
-			}
-		}
-		return result;
-	}
-	@Override
-	public boolean releaseLock(String id, long value){
-		JedisPool pool = null;
-		Jedis	jedis  = null;
-		boolean result = false;
-		try{
-			pool = redisClient.getJedisMasterPool();
-			jedis =	pool.getResource();
-			String key = defaultPrefix + id + defaultSplit + value;
-			long del = jedis.del(key);
-			//删除锁
-			result = del == 1 ? true : false;
-	        if(result) {
-	        	return result;
-	        }
-		}catch(Throwable t){
-			//防御性容错，如果异常直接返回false
-			logger.error(" task id:" + id + " value:" +value , t);
-		}
-		finally{
-			if(pool != null && jedis != null){
-				pool.returnResource(jedis);
-			}
-		}
-		return result;
-	}
 
-	 
-	@Override
-	public String getExecuteAddress(String id,long value) {
-		
-		JedisPool pool = null;
-		Jedis	jedis  = null;
-		String result = null;
-		try{
-			pool = redisClient.getJedisMasterPool();
-			jedis =	pool.getResource();
-			String key = defaultPrefix + id + defaultSplit + value;
-			String s = jedis.get(key);
-			//bug: s may be null if task execute time over the expired time
-			//result = s.substring(0,s.lastIndexOf(defaultSplit));
-			if(s != null){
-				result = s.substring(0,s.lastIndexOf(defaultSplit));
-			}
-		}catch(Throwable t){
-			//防御性容错，如果异常直接返回false
-			logger.error(" task id:" + id + " value:" +value , t);
-		}
-		finally{
-			if(pool != null && jedis != null){
-				pool.returnResource(jedis);
-			}
-		}
-		return result;
-	}
-	/**
-	 * 获取本地的ip地址
-	 * @return
-	 * 		本地地址字符串
-	 */
-	public static String getLocalAddress() {
-    	try {
-    		HostInfo hostInfo = HostInfoHelper.getHostInfo();
-    		if(hostInfo == null || hostInfo.getIpList() == null){
-    			return InetAddress.getLocalHost().toString();
-    		}
-    		IpInfo ipInfo = hostInfo.getIpList().get(NetType.CTL);
-    		if(ipInfo == null){
-    			ipInfo = hostInfo.getIpList().get(NetType.CNII);
-    		}
-    		if(ipInfo != null ){
-    			return ipInfo.getIp();
-    		}
-			return InetAddress.getLocalHost().toString();
-		} catch (UnknownHostException e) {
-			logger.error("getLocalAddress error:{}",e);
-		}
-		return "127.0.0.1";
+    private static final Logger logger = LoggerFactory.getLogger(RedisTaskLock.class);
+
+    private final RedisClient redisClient;
+
+    private static final int defaultExpire = 7 * 24 * 60 * 60; // 失效时间7天
+
+    private static final String defaultPrefix = "cs-base-task-"; //
+
+    private static final String defaultSplit = "&";
+
+    private final int expire; //
+
+    /**
+     * 构造器函数
+     * 
+     * @param redisClient
+     *            {@link RedisClient} redis的java客户端jedis封装类
+     */
+    public RedisTaskLock(RedisClient redisClient) {
+        this(redisClient, defaultExpire);
     }
-    
-//    public static void main(String[] args) {
-//    	String s = "qaz/127.0.0.1&31312";
-//    	Long old = Long.valueOf(s.substring(s.lastIndexOf(defaultSplit)+1));
-//    	System.out.println(s.substring(0,s.lastIndexOf(defaultSplit)));
-//	}
-	
-	
-	
-	
-	
-	
-	
-	
-//	public RedisClient getRedisClient() {
-//		return redisClient;
-//	}
-//
-//	public void setRedisClient(RedisClient redisClient) {
-//		this.redisClient = redisClient;
-//	}
+
+    /**
+     * 构造器函数
+     * 
+     * @param redisClient
+     *            redisClient对象
+     * @param expire
+     *            锁失效时间
+     */
+    public RedisTaskLock(RedisClient redisClient, int expire) {
+        super();
+        this.redisClient = redisClient;
+        this.expire = expire;
+    }
+
+    @Override
+    public boolean lock(String id, long value) {
+        JedisPool pool = null;
+        Jedis jedis = null;
+        boolean result = false;
+        try {
+            pool = redisClient.getJedisMasterPool();
+            jedis = pool.getResource();
+            String key = defaultPrefix + id + defaultSplit + value;
+            String v = getLocalAddress() + defaultSplit + value;
+            long setnx = jedis.setnx(key, v);
+            // 如果等于1 加锁成功
+            result = setnx == 1 ? true : false;
+            if (result) {
+                // 设置超时时间
+                jedis.expire(key, expire);
+                return result;
+            }
+        } catch (Throwable t) {
+            // 防御性容错，如果异常直接返回false
+            logger.error(" task id:" + id + " value:" + value, t);
+        } finally {
+            if (pool != null && jedis != null) {
+                pool.returnResource(jedis);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean releaseLock(String id, long value) {
+        JedisPool pool = null;
+        Jedis jedis = null;
+        boolean result = false;
+        try {
+            pool = redisClient.getJedisMasterPool();
+            jedis = pool.getResource();
+            String key = defaultPrefix + id + defaultSplit + value;
+            long del = jedis.del(key);
+            // 删除锁
+            result = del == 1 ? true : false;
+            if (result) {
+                return result;
+            }
+        } catch (Throwable t) {
+            // 防御性容错，如果异常直接返回false
+            logger.error(" task id:" + id + " value:" + value, t);
+        } finally {
+            if (pool != null && jedis != null) {
+                pool.returnResource(jedis);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String getExecuteAddress(String id, long value) {
+
+        JedisPool pool = null;
+        Jedis jedis = null;
+        String result = null;
+        try {
+            pool = redisClient.getJedisMasterPool();
+            jedis = pool.getResource();
+            String key = defaultPrefix + id + defaultSplit + value;
+            String s = jedis.get(key);
+            // bug: s may be null if task execute time over the expired time
+            // result = s.substring(0,s.lastIndexOf(defaultSplit));
+            if (s != null) {
+                result = s.substring(0, s.lastIndexOf(defaultSplit));
+            }
+        } catch (Throwable t) {
+            // 防御性容错，如果异常直接返回false
+            logger.error(" task id:" + id + " value:" + value, t);
+        } finally {
+            if (pool != null && jedis != null) {
+                pool.returnResource(jedis);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 获取本地的ip地址
+     * 
+     * @return
+     *         本地地址字符串
+     */
+    public static String getLocalAddress() {
+        try {
+            HostInfo hostInfo = HostInfoHelper.getHostInfo();
+            if (hostInfo == null || hostInfo.getIpList() == null) {
+                return InetAddress.getLocalHost().toString();
+            }
+            NetType nt = null;
+            IpInfo ipInfo = null;
+            for (NetType type : NetType.values()) {
+                ipInfo = hostInfo.getIpList().get(type);
+                if (null != ipInfo) {
+                    nt = type;
+                    break;
+                }
+            }
+            if (ipInfo != null) {
+                return ipInfo.getIp() + ":" + nt;
+            }
+            return InetAddress.getLocalHost().toString();
+        } catch (UnknownHostException e) {
+            logger.error("getLocalAddress error:{}", e);
+        }
+        return "127.0.0.1";
+    }
+
+    // public static void main(String[] args) {
+    // String s = "qaz/127.0.0.1&31312";
+    // Long old = Long.valueOf(s.substring(s.lastIndexOf(defaultSplit)+1));
+    // System.out.println(s.substring(0,s.lastIndexOf(defaultSplit)));
+    // }
+
+    // public RedisClient getRedisClient() {
+    // return redisClient;
+    // }
+    //
+    // public void setRedisClient(RedisClient redisClient) {
+    // this.redisClient = redisClient;
+    // }
 
 }
