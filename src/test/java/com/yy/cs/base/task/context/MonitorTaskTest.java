11b6258befcc1f77db21db4ba0fc0d516f996b06
zhupeiquan <zhupeiquan@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2016-02-05 09:41:34 +0000

    task 最近几天日志保存，在 linux 服务器上，无法删除。可以改用使用 logback 按天保存日志的方式，不过这样改动的源码比较多，故不再实现。task 日志的保存，使用会旧的方式
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@307369 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/test/java/com/yy/cs/base/task/context/MonitorTaskTest.java
+++ src/test/java/com/yy/cs/base/task/context/MonitorTaskTest.java
@@ -1,14 +1,7 @@
 package com.yy.cs.base.task.context;
 
 import java.io.File;
-import java.io.FilenameFilter;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 
-import com.yy.cs.base.censor.impl.EmptyWordsFilterImpl;
-import com.yy.cs.base.task.trigger.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,17 +14,17 @@ import com.yy.cs.base.task.context.Constants.MonitorType;
 public class MonitorTaskTest {
 
 	private static final String monitorFile = "D:\\workspace\\monitortask.txt";
-	
-	private static final String monitorFileLog = "/data/monitortask.log";
-	
-	private static final MonitorType monitorType = MonitorType.LOG;
-	
-	private CsStatus csStatus;
-	
 
-/**
- * 初始化csStatus	
- */
+	private static final String monitorFileLog = "D:\\workspace\\monitortask.log";
+
+	private static final MonitorType monitorType = MonitorType.LOG;
+
+	private CsStatus csStatus;
+
+
+	/**
+	 * 初始化csStatus
+	 */
 	private void initCsStatus(){
 		CsStatus subStatusFst = new CsStatus(StatusCode.FAIL,"1111111","Test11111");
 		CsStatus subStatusSed = new CsStatus(StatusCode.FAIL,"2222222","Test22222");
@@ -43,30 +36,30 @@ public class MonitorTaskTest {
 		csStatus.addSubCsStatus(subStatusThd);
 		csStatus.addSubCsStatus(subStatusFth);
 	}
-	
-   /**
-    * 1.用户没有设置monitorFile，文件名由monitorType决定
-    * 	a.monitorType = MonitorType.LOG： 当前路径+"monitortask.log"
-    * 	b.monitorType = MonitorType.HTML：当前路径+"monitortask.html"
-    *  	c.monitorType没有设定，默认为HTML格式输出：当前路径+"monitortask.html"
-    * 2.配置了monitorFile，文件名与monitorFile一致。文件格式同上
-    */
+
+	/**
+	 * 1.用户没有设置monitorFile，文件名由monitorType决定
+	 * 	a.monitorType = MonitorType.LOG： 当前路径+"monitortask.log"
+	 * 	b.monitorType = MonitorType.HTML：当前路径+"monitortask.html"
+	 *  	c.monitorType没有设定，默认为HTML格式输出：当前路径+"monitortask.html"
+	 * 2.配置了monitorFile，文件名与monitorFile一致。文件格式同上
+	 */
 	@Test
-    public void testGetWebPath(){
-    	MonitorTask monitor = new MonitorTask();
-    	MonitorTask monitorB = new MonitorTask("");
-    	MonitorTask monitorF = new MonitorTask(monitorFile);
-    	MonitorTask monitorFT = new MonitorTask(monitorFile, monitorType);
-    	String pathStr = System.getProperty("user.dir") + File.separatorChar;
-    	String path = monitor.getWebPath();
-    	String pathB = monitorB.getWebPath();
-    	String pathF = monitorF.getWebPath();
-    	String pathFT = monitorFT.getWebPath();
-    	Assert.assertNotNull(path);
-    	Assert.assertTrue(pathB.contains(pathStr));
-    	Assert.assertEquals(pathF, monitorFile);
-    	Assert.assertEquals(pathFT, monitorFile);   	
-    }
+	public void testGetWebPath(){
+		MonitorTask monitor = new MonitorTask();
+		MonitorTask monitorB = new MonitorTask("");
+		MonitorTask monitorF = new MonitorTask(monitorFile);
+		MonitorTask monitorFT = new MonitorTask(monitorFile, monitorType);
+		String pathStr = System.getProperty("user.dir") + File.separatorChar;
+		String path = monitor.getWebPath();
+		String pathB = monitorB.getWebPath();
+		String pathF = monitorF.getWebPath();
+		String pathFT = monitorFT.getWebPath();
+		Assert.assertNotNull(path);
+		Assert.assertTrue(pathB.contains(pathStr));
+		Assert.assertEquals(pathF, monitorFile);
+		Assert.assertEquals(pathFT, monitorFile);
+	}
 
 	/**
 	 * 测试生成两种格式的监控文件：LOG/默认HTML格式
@@ -74,23 +67,11 @@ public class MonitorTaskTest {
 	@Test
 	public void testWriteTaskFile(){
 		initCsStatus();
-    	MonitorTask monitorFT = new MonitorTask(null, monitorType);
-    	MonitorTask monitorF = new MonitorTask(monitorFile);
-    	monitorFT.writeTaskFile(csStatus);
-    	monitorF.writeTaskFile(csStatus);
-    	Assert.assertTrue(new File(monitorFile).exists());
-    	Assert.assertTrue(new File(monitorFileLog).exists());
-	}
-
-	@Test
-	public void testDeleteTaskFiles() throws Exception {
-		initCsStatus();
-		MonitorTask monitorFT = new MonitorTask(null, monitorType);
+		MonitorTask monitorFT = new MonitorTask(monitorFileLog, monitorType);
+		MonitorTask monitorF = new MonitorTask(monitorFile);
 		monitorFT.writeTaskFile(csStatus);
-		String logFilePath = monitorFT.getWebPath();
-		File logFile = new File(logFilePath);
-		Assert.assertTrue(logFile.exists());
-//		monitorFT.deleteTaskLogFiles(-1);
-		Assert.assertFalse(logFile.exists());
+		monitorF.writeTaskFile(csStatus);
+		Assert.assertTrue(new File(monitorFile).exists());
+		Assert.assertTrue(new File(monitorFileLog).exists());
 	}
 }
