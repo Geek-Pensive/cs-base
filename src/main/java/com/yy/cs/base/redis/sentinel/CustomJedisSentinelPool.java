b951fd82a0acd8e8a2bfe6c37f5d5e0b9312f9de
chenyouhuang <chenyouhuang@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2016-07-10 12:37:55 +0000

    1、cs-base 增加redis sentinel支持 1）复制to-rank类 2）RedisSentinelFactory 继承 JedisPoolConfigAdapter 3）支持配置中心，要增加属性变量 2、升级jedis版本 3、升级cs-base版本
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@351461 94b0661d-e41e-4e49-82e3-1d5c8131657a

---
+++ src/main/java/com/yy/cs/base/redis/sentinel/CustomJedisSentinelPool.java
@@ -0,0 +1,352 @@
+package com.yy.cs.base.redis.sentinel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.HostAndPort;
+//import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Protocol;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+import redis.clients.util.Pool;
+
+import com.yy.cs.base.json.Json;
+
+public class CustomJedisSentinelPool extends Pool<Jedis> {
+	
+	private static final org.slf4j.Logger log = LoggerFactory.getLogger(CustomJedisSentinelPool.class);
+
+    protected GenericObjectPoolConfig poolConfig;
+
+    protected int timeout = Protocol.DEFAULT_TIMEOUT;
+
+    protected String password;
+
+    protected int database = Protocol.DEFAULT_DATABASE;
+    
+    protected static final String MASTER_PREFIX = "master";
+    
+    protected static final String SLAVE_PREFIX = "slave";
+
+    protected Set<MasterListener> masterListeners = new HashSet<MasterListener>();
+    
+    protected Map <String,ArrayList<HostAndPort>> sentinelsMap = new ConcurrentHashMap<>();
+
+    private Executor executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    
+    private volatile HostAndPort currentHostMaster;
+    
+    private CopyOnWriteArrayList<JedisPool> jedisPools = new CopyOnWriteArrayList<>();
+    
+    public CustomJedisSentinelPool(String masterName, Set<String> sentinels,
+	    final GenericObjectPoolConfig poolConfig) {
+	this(masterName, sentinels, poolConfig, Protocol.DEFAULT_TIMEOUT, null,
+		Protocol.DEFAULT_DATABASE);
+    }
+
+    public CustomJedisSentinelPool(String masterName, Set<String> sentinels) {
+	this(masterName, sentinels, new GenericObjectPoolConfig(),
+		Protocol.DEFAULT_TIMEOUT, null, Protocol.DEFAULT_DATABASE);
+    }
+
+    public CustomJedisSentinelPool(String masterName, Set<String> sentinels,
+	    String password) {
+	this(masterName, sentinels, new GenericObjectPoolConfig(),
+		Protocol.DEFAULT_TIMEOUT, password);
+    }
+
+    public CustomJedisSentinelPool(String masterName, Set<String> sentinels,
+	    final GenericObjectPoolConfig poolConfig, int timeout,
+	    final String password) {
+	this(masterName, sentinels, poolConfig, timeout, password,
+		Protocol.DEFAULT_DATABASE);
+    }
+
+    public CustomJedisSentinelPool(String masterName, Set<String> sentinels,
+	    final GenericObjectPoolConfig poolConfig, final int timeout) {
+	this(masterName, sentinels, poolConfig, timeout, null,
+		Protocol.DEFAULT_DATABASE);
+    }
+
+    public CustomJedisSentinelPool(String masterName, Set<String> sentinels,
+	    final GenericObjectPoolConfig poolConfig, final String password) {
+	this(masterName, sentinels, poolConfig, Protocol.DEFAULT_TIMEOUT,
+		password);
+    }
+
+    public CustomJedisSentinelPool(String masterName, Set<String> sentinels,
+	    final GenericObjectPoolConfig poolConfig, int timeout,
+	    final String password, final int database) {
+		this.poolConfig = poolConfig;
+		this.timeout = timeout;
+		this.password = password;
+		this.database = database;
+		initSentinels(sentinels, masterName,timeout);
+    }
+
+    public void returnBrokenResource(final Jedis resource) {
+    	returnBrokenResourceObject(resource);
+    }
+
+    public void returnResource(final Jedis resource) {
+    	resource.resetState();
+    	returnResourceObject(resource);
+    }
+
+    public void destroy() {
+		for (MasterListener m : masterListeners) {
+		    m.shutdown();
+		}
+		super.destroy();
+		if(jedisPools != null && jedisPools.size() > 0 ){
+    		for(JedisPool pool : jedisPools){
+    			pool.close();
+    		}
+    		jedisPools.clear();
+    	}
+		log.info("CustomJedisSentinelPool destroy...");
+    }
+
+    public HostAndPort getCurrentHostMaster() {
+    	return currentHostMaster;
+    }
+    private void initMasterPool(HostAndPort master) {
+    	//覆写equals，避免重复初始化master pool
+		if (!master.equals(currentHostMaster)) {
+			ArrayList<HostAndPort> ls = new ArrayList<>();
+			ls.add(master);
+			sentinelsMap.put(MASTER_PREFIX, ls);
+		    currentHostMaster = master;
+		    log.info("Created JedisPool to master at " + master);
+		    //创建master pool
+		    initPool(poolConfig,new JedisFactory(master.getHost(), master.getPort(),timeout, password, database));
+		}
+    }
+    
+    private void initSalvePools(ArrayList<HostAndPort> slaves){
+    	try{
+    		lock.writeLock().lock();
+    		boolean isSame = false;
+        	ArrayList<HostAndPort> list = sentinelsMap.get(SLAVE_PREFIX);
+        	if(list != null && slaves != null  
+        		 && slaves.containsAll(list) 
+        		 && list.containsAll(slaves)){
+        		isSame = true;
+        	}
+        	if(isSame){
+        		return ;
+        	}else{
+        		sentinelsMap.put(SLAVE_PREFIX, slaves);
+        	}
+    		if(jedisPools != null && jedisPools.size() > 0 ){
+        		for(JedisPool pool : jedisPools){
+        			log.info("remove and destroy old jedisPool :{}",pool);
+        			pool.close();
+        		}
+        		jedisPools.clear();
+        	}
+        	for (HostAndPort hap : slaves) {
+    			jedisPools.add(new JedisPool(poolConfig, hap.getHost(), hap.getPort(),timeout));
+    			log.info("reload new jedisPool host:{},port:{}",hap.getHost(),hap.getPort());
+    		}	
+    	}finally{
+    		lock.writeLock().unlock();
+    	}
+    }
+    
+    public JedisPool getReaderPool(){
+    	try{
+    		lock.readLock().lock();
+    		int size = jedisPools.size();
+    		if( size > 0 ){
+    			return jedisPools.get(random.nextInt(size));
+    		}else{
+    			log.info("error: none slave pool can be aquired");
+    			return null;
+    		}
+    	}finally{
+    		lock.readLock().unlock();
+    	}
+    }
+    
+    private static final Random random = new Random();
+    
+    private Map<String,ArrayList<HostAndPort>> initSentinels(Set<String> sentinels,
+	    final String masterName,int timeout) {
+    	Map<String,ArrayList<HostAndPort>> map = new HashMap<String,ArrayList<HostAndPort>>();
+		HostAndPort master = null;
+		boolean running = true;
+		 log.info("start initSentinels...");
+		outer: while (running) {
+		    log.info("Trying to find master from available Sentinels...");
+		    for (String sentinel : sentinels) {
+				final HostAndPort hap = toHostAndPort(Arrays.asList(sentinel.split(":")));
+				log.debug("Connecting to Sentinel " + hap);
+				try {
+					    @SuppressWarnings("resource")
+						Jedis jedis = new Jedis(hap.getHost(), hap.getPort(),timeout);
+					    if (master == null) {
+						master = toHostAndPort(jedis.sentinelGetMasterAddrByName(masterName));
+						log.info("found Redis master at " + master);
+						ArrayList<HostAndPort> ls = new ArrayList<>();
+						ls.add(master);
+						sentinelsMap.put(MASTER_PREFIX, ls);
+						//初始化masterPool
+						initMasterPool(master);
+						// 获取从服务器列表
+						ArrayList<HostAndPort> slaves = new ArrayList<>();
+					    for (Map<String, String> slave : jedis.sentinelSlaves(masterName)) {
+					    	HostAndPort _slave = toHostAndPort(Arrays.asList(slave.get("name").split(":")));
+					    	slaves.add(_slave);
+					    	log.info("Found Redis Slave: " + Json.ObjToStr(slave));
+					    }
+						//初始化slavePool
+						initSalvePools(slaves);
+						jedis.close();
+						break outer;
+				    }
+				} catch (JedisConnectionException e) {
+					    log.warn("Cannot connect to sentinel running @ " + hap+ ". Trying next one.");
+				}
+		    }
+		    try {
+				log.error("All sentinels down, cannot determine where is "+ masterName + " master is running... sleeping 1000ms.");
+					Thread.sleep(1000);
+			    } catch (InterruptedException e) {
+			    	e.printStackTrace();
+		    }
+		}
+		log.info("Redis master running at " + master + ", starting Sentinel listeners...");
+		for (String sentinel : sentinels) {
+		    final HostAndPort hap = toHostAndPort(Arrays.asList(sentinel.split(":")));
+		    MasterListener masterListener = new MasterListener(masterName,hap.getHost(), hap.getPort());
+		    masterListeners.add(masterListener);
+		    masterListener.setDaemon(true);
+		    masterListener.start();
+		}
+	
+		return map;
+    }
+
+    private void reloadSlavePools(Jedis jedis,String masterName){
+    	ArrayList<HostAndPort> slaves = new ArrayList<>();
+	    for (Map<String, String> slave : jedis.sentinelSlaves(masterName)) {
+	    	HostAndPort _slave = toHostAndPort(Arrays.asList(slave.get("name").split(":")));
+	    	slaves.add(_slave);
+	    	log.info("reloadSlavePools: found Redis Slave: " + Json.ObjToStr(slave));
+	    }
+	    sentinelsMap.put(SLAVE_PREFIX, slaves);
+	    initSalvePools(slaves);
+	    jedis.close();
+    }
+    
+    private HostAndPort toHostAndPort(List<String> getMasterAddrByNameResult) {
+		String host = getMasterAddrByNameResult.get(0);
+		int port = Integer.parseInt(getMasterAddrByNameResult.get(1));
+		return new HostAndPort(host, port);
+    }
+
+    protected class MasterListener extends Thread {
+
+	protected String masterName;
+	protected String host;
+	protected int port;
+	protected long subscribeRetryWaitTimeMillis = 5000;
+	protected Jedis j;
+	protected AtomicBoolean running = new AtomicBoolean(false);
+
+	protected MasterListener() {
+	}
+
+	public MasterListener(String masterName, String host, int port) {
+	    this.masterName = masterName;
+	    this.host = host;
+	    this.port = port;
+	}
+
+	public MasterListener(String masterName, String host, int port,
+		long subscribeRetryWaitTimeMillis) {
+	    this(masterName, host, port);
+	    this.subscribeRetryWaitTimeMillis = subscribeRetryWaitTimeMillis;
+	}
+
+	public void run() {
+	    running.set(true);
+	    while (running.get()) {
+		try {
+				j = new Jedis(host, port);
+				j.subscribe(new JedisPubSubAdapter() {
+				@Override
+				public void onMessage(String channel, String message) {
+					    log.info("Sentinel " + host + ":" + port+ " published: " + message + ".");
+					    String[] switchMasterMsg = message.split(" ");
+					    if (switchMasterMsg.length > 3) {
+							if (masterName.equals(switchMasterMsg[0])) {
+								HostAndPort hostAddress = toHostAndPort(Arrays.asList(switchMasterMsg[3],switchMasterMsg[4]));
+								log.info("switch master and init pool at :{}",hostAddress);
+								initMasterPool(hostAddress);
+								executorService.execute(new Runnable(){
+									public void run() {
+										reloadSlavePools(new Jedis(host, port), masterName);
+									};
+								});
+							} else {
+							    log.info("Ignoring message on +switch-master for master name "
+								    + switchMasterMsg[0]
+								    + ", our master name is "
+								    + masterName);
+							}
+					    } else {
+					    	log.error("Invalid message received on Sentinel  host:"+ port+ " on channel +switch-master: "+ message);
+					    }
+					}
+			   }, "+switch-master");
+			} catch (JedisConnectionException e) {
+			    if (running.get()) {
+					log.error("Lost connection to Sentinel at " + host
+						+ ":" + port
+						+ ". Sleeping 5000ms and retrying.");
+					try {
+					    Thread.sleep(subscribeRetryWaitTimeMillis);
+					} catch (InterruptedException e1) {
+					    e1.printStackTrace();
+					}
+			    } else {
+			    	log.info("Unsubscribing from Sentinel at " + host + ":"+ port);
+			    }
+			}
+	    }
+	}
+
+	public void shutdown() {
+	    try {
+			log.info("Shutting down listener on " + host + ":" + port);
+			running.set(false);
+			// This isn't good, the Jedis object is not thread safe
+			j.disconnect();
+	    } catch (Exception e) {
+			log.error("Caught exception while shutting down: "
+				+ e.getMessage());
+	    }
+	}
+    }
+
+}
---
+++ src/main/java/com/yy/cs/base/redis/sentinel/RedisSentinelFactory.java
@@ -0,0 +1,196 @@
+package com.yy.cs.base.redis.sentinel;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+import com.yy.cs.base.redis.JedisPoolConfigAdapter;
+
+/**
+ * Redis Sentinel工厂类
+ * 
+ */
+public class RedisSentinelFactory extends JedisPoolConfigAdapter {
+
+	private static final Logger logger = LoggerFactory.getLogger(RedisSentinelFactory.class);
+	
+	private CustomJedisSentinelPool masterPool;
+    private int retries = 3;
+    
+    // 属性变量
+    private String masterName;
+    private Set<String> servers;
+    private String password = null;
+    private int timeout = 8000;
+    
+	
+    public RedisSentinelFactory() {
+        // 初始值
+        this.config.setMaxWaitMillis(6000L);
+        this.config.setTestOnBorrow(true);
+    }
+
+	/**
+	 * 初始化
+	 */
+	public void init() {
+	    
+	    checkArguments();
+	    
+        password = "".equals(password) ? null : password;
+	    masterPool = new CustomJedisSentinelPool(masterName, servers, this.config,timeout,password);
+	}
+	
+	private void checkArguments(){
+	    if(servers == null || servers.size() < 0){
+            throw new IllegalArgumentException("sentinel server configs "
+                    + "sentinelServers:{"+servers+"} should not be null");
+        }
+	    
+	    if(masterName == null || "".equals(masterName.trim())){
+	        throw new IllegalArgumentException("masterName config should not be null");
+	    }
+	}
+	
+	/**
+	 * 获取Master实例
+	 * 
+	 * @return
+	 */
+	public Jedis getMaster() {
+		for (int i = 0; i <= retries; i++) {
+			Jedis jedis = null;
+			try {
+				jedis = masterPool.getResource();
+				return jedis;
+			} catch (JedisConnectionException jce) {
+				logger.error("[getMaster] sentinelPool.getResource() error", jce);
+				if (jedis != null) {
+					masterPool.returnBrokenResource(jedis);
+					jedis = null;
+				}
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 从Jedis池中获取1个Pool
+	 * 
+	 * @return
+	 */
+	public JedisPool getReaderPool() {
+		JedisPool pool = null;
+		for(int i = 0 ; i < retries ; i++ ){
+			pool = masterPool.getReaderPool();
+			if(pool != null){
+				return pool;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 获取只读Jedis实例
+	 * 
+	 * @return
+	 */
+	public Jedis getReader() {
+			JedisPool readPool=null;
+			Jedis jedis = null;
+			try {
+				//从不同的实例拿
+				for(int i = 0 ; i < retries ; i++ ){
+					try{
+						readPool = getReaderPool();
+						if(readPool != null ){
+							jedis = readPool.getResource();
+						}
+						if(jedis != null ){
+							break;
+						}
+					}catch(JedisConnectionException e){
+						if(readPool != null ){
+							readPool.returnBrokenResource(jedis);
+							logger.warn("get jedis instance timeout from pool failure,try next slave,error:{}",e.getCause());
+						}
+					}
+				}
+				//从 master 拿
+				if(jedis == null ){
+					jedis = masterPool.getResource();
+				}
+				if(jedis == null){
+					logger.error(" neither master pool nor slave pool can get jedis instance");
+				}else{
+					return jedis;
+				}
+			} catch (JedisConnectionException jce) {
+				logger.error("[getReader] jedisPool.getResource() error:{}", jce.getCause());
+				if (jedis != null) {
+					jedis.close();
+				}
+			}
+			return null;
+		
+	}
+	
+	public void returnSentinelResource(Jedis jedis) {
+		masterPool.returnResource(jedis);
+	}
+	
+	public void returnBrokenResource(Jedis jedis) {
+		masterPool.returnBrokenResource(jedis);
+	}
+	
+	public void returnJedisResource(Jedis jedis) {
+		if(jedis != null )
+			jedis.close();
+	}
+	
+	public void destroy() {
+		if (masterPool != null) {
+			masterPool.destroy();
+		}
+	}
+
+	
+    public String getMasterName() {
+        return masterName;
+    }
+
+    public void setMasterName(String masterName) {
+        this.masterName = masterName;
+    }
+
+    public Set<String> getServers() {
+        return servers;
+    }
+
+    public void setServers(Set<String> servers) {
+        this.servers = servers;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+	
+}
