cfd9187d58baff15a50ed4d403d9db2897b34d07
tanghengde <tanghengde@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2016-08-12 02:24:57 +0000

    删除RedisClientFactory初始化时没有主库会把从库当做主库的逻辑。
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@362226 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/main/java/com/yy/cs/base/redis/RedisClientFactory.java
+++ src/main/java/com/yy/cs/base/redis/RedisClientFactory.java
@@ -229,10 +229,6 @@ public class RedisClientFactory extends JedisPoolConfigAdapter {
             List<JedisPool> oldRslavePool = redisSlavePool;
             redisMasterPool = newMasterPool;
             redisSlavePool = newRslavePool;
-            // 如果没有master 避免用户直接获取master进行操作导致错误
-            if (redisMasterPool.size() == 0) {
-                redisMasterPool = redisSlavePool;
-            }
             // 如果没有slave 避免用户直接获取slave进行操作导致错误
             if (redisSlavePool.size() == 0) {
                 redisSlavePool = redisMasterPool;
