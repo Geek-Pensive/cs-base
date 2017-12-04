eea96f07c2527981a3133027d8ccb958f5d47710
tanghengde <tanghengde@94b0661d-e41e-4e49-82e3-1d5c8131657a>
2017-12-04 06:33:39 +0000

    import redis poolConfig
    git-svn-id: https://svn.yy.com/web/gh/cs/trunk/cs-base@504116 94b0661d-e41e-4e49-82e3-1d5c8131657a

--- src/main/java/com/yy/cs/base/redis/sentinel/SlaveJedisPool.java
+++ src/main/java/com/yy/cs/base/redis/sentinel/SlaveJedisPool.java
@@ -1,9 +1,8 @@
 package com.yy.cs.base.redis.sentinel;
 
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
-
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
 
 /**
@@ -15,7 +14,7 @@ public class SlaveJedisPool extends JedisPool {
 
     private final HostAndPort hostAndPort;
 
-    public SlaveJedisPool(final GenericObjectPoolConfig poolConfig, final HostAndPort hostAndPort, final int timeout) {
+    public SlaveJedisPool(final JedisPoolConfig poolConfig, final HostAndPort hostAndPort, final int timeout) {
         super(poolConfig, hostAndPort.getHost(), hostAndPort.getPort(), timeout, null, Protocol.DEFAULT_DATABASE, null);
         this.hostAndPort = hostAndPort;
 
